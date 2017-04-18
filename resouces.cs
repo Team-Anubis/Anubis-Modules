333f7bcd794ae817a0ff3c806abd1f9bd1c30456
Riley Shelton <rs854@nau.edu>
2017-04-17 17:47:41 -0700

    Finished the Resources Page

---
+++ resouces.cs
@@ -0,0 +1,26 @@
+@using DotNetNuke.Common;
+
+@*Copyright (c) 2017 by Anubis *@
+
+<body>
+
+<h2>The Findlay Toyota Pet Resource line</h2>
+<h2><a href="tel:1 (928) 300-4510">1 (928) 300-4510</a></h2>
+<br></br>
+<h2>Vouchers for low-cost and no-cost spay/neuter surgeries</h2>
+<p>The number one way to ensure a high quality of life for the companion animals in our region and reduce the number of homeless pets is to prevent unwanted litters of puppies and kittens. Paw Placement of Northern Arizona, the Flagstaff Community Foundation, and local participating veterinarians are working together to help eliminate pet overpopulation. If you have a cat or dog that needs to be spayed or neutered, and you cannot afford to pay on your own, please call <a href="tel:1 (928) 300-4510">1 (928) 300-4510</a> to request a voucher for a no-cost or low-cost spay or neuter surgery.</p>
+<br></br>
+<h2>Financial assistance for <span class="text-danger">emergency vet</span> care</h2>
+<p>For many, caring for a beloved pet after an accident, injury, or unexpected illness may be more than they can afford. Sometimes this results in euthanasia of the animal or turning the animal in to a shelter. For those owners who qualify, PPNAZ can help defray some of those costs from a fund supported by the Arizona Community Foundation and private donations. PPNAZ works with specific local veterinarians in these situations. For more information about this program, call <a href="tel:1 (928) 699-7586">1 (928) 699-7586</a>.</p>
+<br></br>
+<h2>Education Programs</h2>
+<p>Education programs include informing the public (community, schools, media) about the importance of spaying and neutering in preventing litters of unwanted kittens and puppies as well as the care and well-being of pets. Education also plays a critical role in helping families keep their pets who may have behavioral issues. PPNAZ educational programs include participating in the national Fix At Four campaign and visits to schools and community groups. PPNAZ volunteers also work with individuals and families who have pets with behavioral issues to prevent those pets from being taken to a shelter. Volunteers will identify experts in the community who can help address the identified issues. </p>
+<br></br>
+<h2>Community Cats Program</h2>
+<p>he Community Cats Program provides alternatives to euthanasia through the Trap/Neuter/Return (TNR) and working cats initiatives. In this program, PPNAZ collaborates with Main Street Strays and ADLA in their TNR efforts.
+We also work with the Coconino Humane Association to identify cats eligible for the working cats program where community and business partners want to employ a cat(s) to protect their property from rodents with an environmentally safe alternative to poisonous pest control or traps. If you are interested in "hiring" one or more cats for your business, warehouse, barn, or other property, please review our <a href="http://www.ppnaz.org/brochure">brochure</a> for detailed information. Then call <a href="tel:1 (928) 699-7586">1 (928) 699-7586</a> for an application to protect your property and save the life of a homeless cat. </p>
+<br></br>
+<h2>Pet Food Bank</h2>
+<p>The Coconino Humane Association (CHA) Pet Food Bank provides pet food as a temporary resource to eligible Coconino County residents. The program is meant to supplement the pet's food supply and may not fulfill all the dietary needs of the pet. Food is distributed on the third Saturday of the month at the Coconino Humane Association. Completion of an <a href="http://www.ppnaz.org/foodbankapp">application</a> is required to be considered for eligibility. Food is made possible by donations from individuals and businesses in our community. Paw Placement supports the CHA Pet Food Bank by assisting with boxing/bagging the food and distributing it each month.</p>
+
+</body>
